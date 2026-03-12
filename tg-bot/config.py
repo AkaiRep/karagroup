1c9cddb5825799ecd7067569bdf1d3eb82ee2882
AkaiRep <akaipureya@ya.ru>
2026-03-12 17:41:42 +0100

    update 2026-03-12 17:41

--- tg-bot/config.py
+++ tg-bot/config.py
@@ -13,9 +13,7 @@ class Settings(BaseSettings):
     TERMS_URL: str = "https://telegra.ph/terms"
     PRIVACY_URL: str = "https://telegra.ph/privacy"
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "ignore"}
 
 
 settings = Settings()
