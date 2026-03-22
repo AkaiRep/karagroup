cc7ab23170fbe9a6c3de36158a1ae709fe540bc2
AkaiRep <akaipureya@ya.ru>
2026-03-22 17:57:54 +0100

    update 2026-03-22 17:57

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,23 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy.pool import NullPool
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./karagroup.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
-    poolclass=NullPool,
+    pool_size=20,
+    max_overflow=40,
+    pool_timeout=30,
 )
+
+
+@event.listens_for(engine, "connect")
+def _set_sqlite_pragmas(dbapi_conn, _):
+    cur = dbapi_conn.cursor()
+    cur.execute("PRAGMA journal_mode=WAL")
+    cur.execute("PRAGMA busy_timeout=5000")
+    cur.close()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
